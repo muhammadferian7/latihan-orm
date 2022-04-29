e8d3a1b4e5f1c782953252bf0c46704062a8c8a5
Muhammad Ferian Rizky Akbari <muhammadferian@gmail.com>
2022-04-29 12:58:11 +0700

    add check jwt token

---
+++ middleware/checkToken.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken')
+
+const checkToken = (req, res, next) => {
+  let token = req.headers.authorization
+
+  if (!token) {
+    return res.status(403).json({
+        "success" : false,
+        "error" : 403,
+        "message" : 'please provide a token',
+        "data" : null
+    })
+  }
+
+  // if provided with Bearer then remove it
+  if (token.toLowerCase().startsWith('bearer')) {
+    token = token.slice('bearer'.length).trim()
+  }
+
+  try {
+    const jwtPayload = jwt.verify(token, 'password!23')
+
+    if (!jwtPayload) { 
+        return res.status(403).json({
+            "success" : false,
+            "error" : 403,
+            "message" : 'unauthorized',
+            "data" : null
+        })
+    }
+
+    res.locals.user = jwtPayload
+
+    next()
+  } catch (error) {
+    return res.status(403).json({
+        "success" : false,
+        "error" : 403,
+        "message" : 'failed to check token',
+        "data" : null
+    })
+  }
+}
+
+module.exports = checkToken
--- router/category.js
+++ router/category.js
@@ -3,10 +3,11 @@ const router = express.Router()
 const { list, create, update, destroy } = require('../controller/categoryController.js')
 const validate = require ('../middleware/validate.js')
 const { createCategoryRules } = require('../validators/rule.js')
+const checkToken = require('../middleware/checkToken.js')
 
-router.get('/list', list)
-router.post('/create', validate(createCategoryRules), create)
-router.put('/update', update)
-router.delete('/destroy', destroy)
+router.get('/list', checkToken, list)
+router.post('/create', checkToken, validate(createCategoryRules), create)
+router.put('/update', checkToken, update)
+router.delete('/destroy', checkToken, destroy)
 
 module.exports = router
